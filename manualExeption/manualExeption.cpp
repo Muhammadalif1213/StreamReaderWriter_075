389d8965276b30d898e04dfed015e0044bfa14ed
Muhammadalif1213 <muhammadalif1213@gmail.com>
2023-06-21 11:30:29 +0700

    menambahkan catch, apabila tanpa ketentuan integer maka akan di eksekusi

--- manualExeption/manualExeption.cpp
+++ manualExeption/manualExeption.cpp
@@ -12,5 +12,9 @@ int main()
 		//blok ini akan dieksekusi
 		cout << "pengecualian akan di eksekusi" << endl;
 	}
+	catch (...) {
+		//Jika selain integer maka block ini akan dieksesusi
+		cout << "Default Pengecualian dieksekusi" << endl;
+	}
 
 }
