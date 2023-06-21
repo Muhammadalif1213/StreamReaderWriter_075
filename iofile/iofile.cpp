741ed1999724979f9e35858df1b2977700a07df6
Muhammadalif1213 <muhammadalif1213@gmail.com>
2023-06-21 10:28:24 +0700

    membuat object outfile

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -4,5 +4,14 @@
 using namespace std;
 
 int main() {
+	string baris;
 
+	// membuka file dalam mode menulis.
+	ofstream outfile;
+	// menunjuk ke sebuah file
+	outfile.open("contohfile.txt");
+
+
+
+	
 }
