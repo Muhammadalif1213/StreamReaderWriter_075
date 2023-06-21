eb284dee3a8dd6814cc20442665c78678235a602
Muhammadalif1213 <muhammadalif1213@gmail.com>
2023-06-21 11:33:50 +0700

    membuat int main

---
+++ exeptionStandard/exeptionStandard.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <exception>
+//untuk object exeption yang akan digunakan 
+#include <array>
+//untuk object array yang akan kita gunakan
+using namespace std;
+int main() {
+
+}
